8685c4bc873023b444a3ac82815cfc6d621e4fde
Jonas Bourguignon <72096294+Xormi0s@users.noreply.github.com>
2024-05-07 15:15:18 +0200

    Expense Tracker part 2

--- PyQT course/Expense Tracker/main.py
+++ PyQT course/Expense Tracker/main.py
@@ -1,6 +1,7 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QLineEdit, QComboBox, QDateEdit, QTableWidget, QHBoxLayout, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QLineEdit, QComboBox, QDateEdit, QTableWidget, QHBoxLayout, QVBoxLayout, QMessageBox, QTableWidgetItem
 from PyQt5.QtCore import Qt
-
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery
+import sys
 
 class ExpenseApp(QWidget):
     def __init__(self):
@@ -20,6 +21,8 @@ class ExpenseApp(QWidget):
         self.table.setColumnCount(5)
         header_names = ["Id", "Date", "Category", "Amount", "Description"]
         self.table.setHorizontalHeaderLabels(header_names)
+
+        self.dropdown.addItems(["Food", "Transportation", "Rent", "Shopping", "Movies", "Other"])
 
         self.master_layout = QVBoxLayout()
         self.row1 = QHBoxLayout()
@@ -46,6 +49,55 @@ class ExpenseApp(QWidget):
         self.master_layout.addWidget(self.table)
 
         self.setLayout(self.master_layout)
+
+        database = QSqlDatabase.addDatabase("QSQLITE")
+        database.setDatabaseName("expense.db")
+        if not database.open():
+            QMessageBox.critical(None, "Error", "Could not open your database")
+            print("MEH")
+            sys.exit(1)
+
+        query = QSqlQuery()
+        query.exec("""
+                            CREATE TABLE IF NOT EXISTS expenses (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                date TEXT,
+                                category TEXT,
+                                amount REAL,
+                                description TEXT
+                            )
+                            """)
+
+        self.load_table()
+
+    def load_table(self):
+        self.table.setRowCount(0)
+        query = QSqlQuery("SELECT * FROM expenses")
+        row = 0
+        while query.next():
+            expense_id = query.value(0)
+            date = query.value(1)
+            category = query.value(2)
+            amount = query.value(3)
+            description = query.value(4)
+
+            self.table.insertRow(row)
+            self.table.setItem(row, 0, QTableWidgetItem(str(expense_id)))
+            self.table.setItem(row, 1, QTableWidgetItem(str(date)))
+            self.table.setItem(row, 2, QTableWidgetItem(str(category)))
+            self.table.setItem(row, 3, QTableWidgetItem(str(amount)))
+            self.table.setItem(row, 4, QTableWidgetItem(str(description)))
+
+            row += 1
+
+    def add_expense(self):
+        date = self.date_box.date().toString("yyyy-MM-dd")
+        category = self.dropdown.currentText()
+        amount = self.amount.text()
+        description = self.description.text()
+
+        query = QSqlQuery()
+        query.pre
 
 
 if __name__ in "__main__":
